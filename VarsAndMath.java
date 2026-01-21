bf30ae7657e496031f4441588fc58c80d8188e8c
Danny Rivera <DannRive4IT@gmail.com>
2026-01-21 11:58:19 -0800

    Add VarsAndMath class with main method

---
+++ VarsAndMath.java
@@ -0,0 +1,10 @@
+// References: 
+// https://www.w3schools.com/java/java_variables.asp
+// https://www.w3schools.com/java/java_operators.asp
+
+public class VarsAndMath {
+    public static void main(String[] args) {
+        // System.out.println(firstName + " bought " + items + " items.");
+System.out.println("The total cost was: $" + total);
+    }
+}
