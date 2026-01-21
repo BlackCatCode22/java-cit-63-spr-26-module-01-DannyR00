d8c87314c16ca0c60a1b3322f7b5b4b2f333ca29
Danny Rivera <DannRive4IT@gmail.com>
2026-01-21 11:56:14 -0800

    Add HelloWorld class with main method

---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+// URL: https://www.w3schools.com/java/java_syntax.asp
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Use the system's standard output stream to print text
+        System.out.println("Hello World!");
+    }
+}
